06fc189f09d12e42a0ec7f4a6c7e2714947bdeb1
goyerleo <goyer@et.esiea.fr>
2024-01-10 15:27:14 +0100

    finition

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    Collision2D other;
+    [SerializeField] GameObject canvas;
+
+    public static Trap instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.SetActive(false);
+    }
+
+    private void Awake()
+    {
+        instance = this; // Initialisez la r�f�rence instance
+    }
+
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.GetComponent<Collider>().CompareTag("Player"))
+        {
+            Debug.Log("hehe");
+            canvas.SetActive(true);
+        }
+    }
+}
